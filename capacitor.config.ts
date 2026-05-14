b75ac870f1452375f01d0ee771ce53651fd7aacf
DB3 Apps <dusty.black7520@gmail.com>
2026-05-13 21:42:08 -0400

    feat: Integrate Capacitor for native builds
    Adds Capacitor integration to enable native mobile builds for iOS and Android. This includes updating project dependencies, configuring Capacitor, and setting up necessary Android project files.

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.stonkproof.research',
+  appName: 'StonkProof Research',
+  webDir: 'build'
+};
+
+export default config;
